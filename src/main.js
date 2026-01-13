5cf9a5ccbcb462d2ad2810f29d7221fa775f3ddb
Tomatsuppe11211 <marcos.lindboe47@gmail.com>
2026-01-13 16:23:39 +0100

    added contact form
    Added a contact form. missing spinning loading icon for when submitting the form.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,54 @@
-//Getting HTML-elements
+//Getting HTML-elements for login
 const loginform = document.getElementById('loginForm')
-const message = document.getElementById('message')
+const loginMessage = document.getElementById('loginMessage')
 const typedEmail = document.getElementById('email')
 const typedPassword = document.getElementById('password')
 const button = document.getElementById('button')
 
 
-//Setting user
+//Setting user for login
 let name = 'John Doe'
 let email = 'johndoe@example.com'
 let password = 'Person'
 
 
-//submit function
+//submit login function
 function login(){
     if(typedEmail.value === email && typedPassword.value === password){
-        message.innerHTML = `Logged in successfully! Welcome back ${name}`
-        message.className = 'alert alert-success d-flex w-50'
+        loginMessage.innerHTML = `Logged in successfully! Welcome back ${name}`
+        loginMessage.className = 'alert alert-success d-flex w-50'
     } else {
-        message.innerHTML = 'Incorrect email or password. Please try again'
-        message.className = 'alert alert-danger d-flex w-50'
+        loginMessage.innerHTML = 'Incorrect email or password. Please try again'
+        loginMessage.className = 'alert alert-danger d-flex w-50'
     }
 }
-
 
 loginform.addEventListener('submit', function(e){
     e.preventDefault()
     login()
+})
+
+
+
+
+
+
+
+
+
+//Getting HTML-elements for contact
+const contactForm = document.getElementById('contactForm')
+const contactMessage = document.getElementById('contactMessage')
+const contactButton = document.getElementById('contactButton')
+
+
+function sendMessage(){
+    contactMessage.className = 'alert alert-success d-flex w-50'
+    contactMessage.innerHTML = 'Message sent'
+}
+
+
+contactForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    sendMessage()
 })
