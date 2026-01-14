d5e20e965d4427f55aa2a337850833a626ae9092
Tomatsuppe11211 <marcos.lindboe47@gmail.com>
2026-01-14 14:12:55 +0100

    Adding popover

--- src/main.js
+++ src/main.js
@@ -93,3 +93,10 @@ contactForm.addEventListener('submit', function(e){
 //adding tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+//Adding popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
